f32b4cb5c7ba00561e991dc5be3c511d39f12242
Toosdai Otte <toosdai52Otte@gmail.com>
2019-11-11 15:11:22 -0600

    GET actions request working

--- data/helpers/actionRouter.js
+++ data/helpers/actionRouter.js
@@ -5,7 +5,16 @@ const Actions = require('./actionModel.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+    Actions.get()
+    .then(actions => {
+        res.status(200).json(actions)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: 'Error retrieving actions',
+            err
+        })
+    })
 });
 
 router.get('/:id', (req, res) => {
@@ -16,12 +25,14 @@ router.post('/:id', (req, res) => {
 
 });
 
+router.put('/:id', (req, res) => {
+
+});
+
 router.delete('/:id', (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
 
-});
 
 module.exports = router;
